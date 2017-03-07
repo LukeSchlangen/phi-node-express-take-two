ef16ac2733590b2c1ab985671897674576852648
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-06 20:15:39 -0600

    cleaning app.js

--- server/app.js
+++ server/app.js
@@ -4,11 +4,15 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fish = require('./routes/fish');
+var port = 5000;
 
 app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true})); // this creates req.body
+app.use(bodyParser.json()); // this creates req.body
 
 app.use('/fish', fish);
 
-app.listen(5000);
+app.listen(port, function(){
+    console.log('Listening on port: ', port);
+});
