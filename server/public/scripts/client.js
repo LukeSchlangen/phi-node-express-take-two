7fb5875995dbea164ae318f56c22b1288b383d40
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-06 20:16:18 -0600

    angular conversion

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,41 +1,28 @@
-console.log('sourced!');
-$(document).ready(function(){
-  console.log('jquery was correctly sourced!');
+var myApp = angular.module('FishApp', []);
+
+myApp.controller('FishController', ['$http', function ($http) {
+  var self = this;
+  self.fishList = [];
+  self.newFish = {};
   getFishData();
   function getFishData() {
-    $.ajax({
-      type: 'GET',
-      url: '/fish',
-      success: function(response) {
-        console.log('response', response);
-        $('#fishTank').empty();
-        for (var i = 0; i < response.length; i++) {
-          $('#fishTank').append('<li>' + response[i].name + '</li>');
-        }
-      }
-    });
-
-    $.ajax({
-      type: 'GET',
-      url: '/fish/first/name',
-      success: function(response) {
-        console.log('response', response);
-        $('#firstFishy').text(response);
-      }
+    $http({
+      method: 'GET',
+      url: '/fish'
+    }).then(function (response) {
+      console.log('response', response);
+      self.fishList = response.data;
     });
   }
 
-  $('#newFishButton').on('click', function(){
-    var newFishObject = {};
-    newFishObject.name = $('#newFishName').val();
-    $.ajax({
-      type: 'POST',
+  self.addNewFish = function () {
+    $http({
+      method: 'POST',
       url: '/fish/new',
-      data: newFishObject,
-      success: function(response){
-        console.log(response);
-        getFishData();
-      }
+      data: self.newFish
+    }).then(function (response) {
+      console.log(response);
+      getFishData();
     });
-  });
-});
+  }
+}]);
